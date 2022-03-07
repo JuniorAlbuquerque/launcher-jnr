f78dbf3aa37162a55edcd71be6ab02689b5bace6
Junior Albuquerque <junioralbuquerque81@gmail.com>
2022-03-06 20:13:11 -0400

    chore: config svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+const svgrPlugin = require('vite-plugin-svgr')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    svgrPlugin({
+      svgrOptions: {
+        icon: true
+      }
+    }),
     react({
       jsxImportSource: '@emotion/react',
       babel: {
