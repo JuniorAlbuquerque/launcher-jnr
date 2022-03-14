02f42b30e264c066f1b0cf79e30ddd53cc8f892f
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2022-03-13 21:33:18 -0300

    chore: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import { Fragment } from 'react'
 import { globalStyles } from './styles/globalStyles'
 import { theme } from './theme'
 import { Routes } from './routes'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <Fragment>
       <Global styles={globalStyles} />
       <ThemeProvider theme={theme}>
-        <Routes />
+        <QueryClientProvider client={queryClient}>
+          <Routes />
+        </QueryClientProvider>
       </ThemeProvider>
     </Fragment>
   )
